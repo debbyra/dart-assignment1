19aa8575b1dfef9d0117f0af3f9dccf05e216f00
debbyra <deborahrahj20@gmail.com>
2023-09-22 14:11:13 +0300

    dart-assignment1

--- number6.dart
+++ number6.dart
@@ -1,9 +1,11 @@
 // Write a dart program to get a difference between a given number and seventeen,
 // if the number is greater than seventeen return an absolute difference.
 
+import "dart:io";
+
 void main() {
   // Get the user input for the number
-  print("Enter a number: ");
+  stdout.write("Enter a number: ");
   double number = double.parse(stdin.readLineSync()!);
 
   // Calculate the absolute difference
